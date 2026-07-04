53e466d79e823cb2cea59572f9e3cc0c66f43dd2
05joohye-create <05.joohye@gmail.com>
2026-07-04 10:23:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+function addPlace() {
+    const input = document.getElementById("placeInput");
+    const place = input.value.trim();
 
+    if (place === "") {
+        alert("여행지를 입력하세요!");
+        return;
+    }
+
+    const list = document.getElementById("placeList");
+
+    const li = document.createElement("li");
+
+    const text = document.createElement("span");
+    text.textContent = "✈️ " + place;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "삭제";
+
+    deleteBtn.onclick = function () {
+        li.remove();
+    };
+
+    li.appendChild(text);
+    li.appendChild(deleteBtn);
+
+    list.appendChild(li);
+
+    input.value = "";
+    input.focus();
+}
+
+// 엔터키로도 추가
+document.getElementById("placeInput").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addPlace();
+    }
+});
